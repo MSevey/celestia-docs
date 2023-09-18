b32cc326ac2dc2690b6bb6b1502af822d7784fd0
MSevey <MSevey@users.noreply.github.com>
2023-09-18 14:29:59 +0000

    Deploy preview for PR 2 🛫

---
+++ pr-preview/pr-2/assets/js/7111c707.22d88fbe.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcelestia_docs=self.webpackChunkcelestia_docs||[]).push([[1635],{1880:e=>{e.exports=JSON.parse('{"title":"How Celestia works","slug":"/category/how-celestia-works","permalink":"/pr-preview/pr-2/category/how-celestia-works","navigation":{"next":{"title":"Introduction","permalink":"/pr-preview/pr-2/learn/how-celestia-works/introduction"}}}')}}]);
